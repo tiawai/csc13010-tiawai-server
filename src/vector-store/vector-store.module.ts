5c8a8f4c8a0ec5b937a5d2d6f6e6f26e722354b0
Nguyen Duy Hoang <ndhoang22@clc.fitus.edu.vn>
2025-04-16 16:32:08 +0700

    feat: implement SearchQueryDto and VectorStoreController for search functionality

--- src/vector-store/vector-store.module.ts
+++ src/vector-store/vector-store.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VectorStoreService } from './vector-store.service';
+import { VectorStoreController } from './vector-store.controller';
 
 @Module({
+    controllers: [VectorStoreController],
     providers: [VectorStoreService],
     exports: [VectorStoreService],
 })
